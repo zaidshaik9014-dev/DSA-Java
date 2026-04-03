32b83917b371ef3bf2e2224780c217b45942b97d
zaidshaik9014-dev <zaidshaik9014@gmail.com>
2026-04-03 09:52:23 +0530

    added comments

--- Sorting Techniques/QuickSort.java
+++ Sorting Techniques/QuickSort.java
@@ -1,11 +1,11 @@
 public class QuickSort {
     public static int partition(int a[], int low, int high) {
-        int pivot = a[high];
-        int i = low - 1;
+        int pivot = a[high];//arrays last ele
+        int i = low - 1;//which helps to track elements mt places < pivot to add small ele in that places
 
         for (int j = low; j < high; j++) {
             if (a[j] < pivot) {
-                i++;
+                i++;//it tracks empty spaces for placing ele < pivot
                 //swap
                 int temp = a[i];
                 a[i] = a[j];
@@ -16,15 +16,15 @@ public class QuickSort {
         int temp = a[i];
         a[i] = pivot;
         a[high] = temp;
-        return i;
+        return i;//pivot index
     }
 
     public static void quickSort(int a[], int low, int high) {
         if (low < high) {
-            int pidx = partition(a, low, high);
+            int pidx = partition(a, low, high);//divide the array
 
-            quickSort(a, low, pidx - 1);
-            quickSort(a, pidx + 1, high);
+            quickSort(a, low, pidx - 1);//sort ele < pivot
+            quickSort(a, pidx + 1, high);//sort ele > pivot
         }
     }
     public static void main(String[] args) {
