edf62588d2fc6dc6b0aa01248ddfd5cb86dd52e7
zaidshaik9014-dev <zaidshaik9014@gmail.com>
2026-04-09 18:18:50 +0530

    New Prblms

---
+++ Arrays/Sliding Window/FirstNegativeNoQueue.java
@@ -0,0 +1,25 @@
+public class FirstNegativeNoQueue {
+    public static void printFirstNegetive(int[] a, int k) {
+        for (int i = 0; i <= a.length - k; i++) {
+            boolean found = false;
+
+            for (int j = i; j < i + k; j++) {
+                if (a[j] < 0) {
+                    System.out.print(a[j] + " ");
+                    found = true;
+                    break;
+                }
+            }
+            
+            if (!found) {
+                System.out.print("0 ");
+            }
+        }
+    }
+        public static void main(String[] args) {
+        int[] a = {12, -1, -7, 8, -15, 30, 16, 28};
+        int k = 3;
+
+        printFirstNegetive(a, k);
+    }
+}
