5bfc719abcc71fa0327e5ccba9db1681140960dc
zaidshaik9014-dev <zaidshaik9014@gmail.com>
2026-04-16 17:59:15 +0530

    Solving two pointers

---
+++ Arrays/Two Pointers/TwoSumII.java
@@ -0,0 +1,27 @@
+public class TwoSumII {
+    public static int[] twoSum(int[] numbers, int target) {
+        int left = 0, right = numbers.length - 1;
+        
+        while (left < right) {
+            int sum = numbers[left] + numbers[right];
+
+            if (sum == target) {
+                return new int[]{left + 1, right + 1};
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return new int[]{-1, -1};
+    }
+    public static void main(String[] args) {
+        int[] numbers = {2, 7, 11, 15};
+        int target = 9;
+
+        int[] result = twoSum(numbers, target);
+
+        System.out.println("Indices: " + result[0] + " " + result[1]);
+    }
+}
